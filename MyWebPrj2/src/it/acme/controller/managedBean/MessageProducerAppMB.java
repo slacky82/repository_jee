1038a17979114045eac14f4bbccc2d431afdb154
slacky82 <fabriziogerina@gmail.com>
2014-11-18 12:32:24 +0100

    eclp

--- MyWebPrj2/src/it/acme/controller/managedBean/MessageProducerAppMB.java
+++ MyWebPrj2/src/it/acme/controller/managedBean/MessageProducerAppMB.java
@@ -1,8 +1,11 @@
 package it.acme.controller.managedBean;
 
 
+import java.util.Properties;
+
 import it.acme.ejb.jms.MessageProducerBeanRemote;
 import it.acme.ejb.stateless.FilterStrategyFacadeRemote;
+import it.acme.ejb.stateless.MyStatelessSessionBeanRemote;
 import it.acme.exception.BusinessException;
 
 import javax.annotation.Resource;
@@ -14,12 +17,16 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 
 @ManagedBean(name="messageProducerApp")
@@ -45,9 +52,8 @@ public class MessageProducerAppMB {
     	
     }
     
-    public void sendMessage(ActionEvent event){     	
-    	
-         try {
+    public void sendMessage(ActionEvent event){    	
+         try {        	 
              sendJMSMessageToHelloWorldQueue(message);  
              createFacesMessage(message,false);
          } catch (JMSException ex) {
@@ -55,6 +61,52 @@ public class MessageProducerAppMB {
              System.err.println("MessageProducerApp.main: failed to send message - " + ex);
          }   	
 	}
+   
+    public void sendJMSMessageToHelloWorldQueueByLookup(ActionEvent event) throws JMSException {
+		Connection connection = null;
+		Session session = null;
+		Context context = null;
+		Properties properties = new Properties();
+		// properties.put(Context.INITIAL_CONTEXT_FACTORY,"weblogic.jndi.WLInitialContextFactory");
+		//properties.put(Context.INITIAL_CONTEXT_FACTORY,"com.sun.enterprise.naming.impl.SerialInitContextFactory");
+		//properties.put(Context.SECURITY_PRINCIPAL, "weblogic");
+		//properties.put(Context.SECURITY_CREDENTIALS, "weblogic1");
+		//properties.put(Context.PROVIDER_URL, "t3://localhost:7001");
+		try {
+			context = new InitialContext(properties);
+			//ConnectionFactory conFactory = (ConnectionFactory) context.lookup("java: comp/env/jms/HelloWorldQueueFactory");
+			
+			ConnectionFactory connFactory = (ConnectionFactory) context.lookup("jms/HelloWorldQueueFactory");
+			Destination queueDestination = (Destination) context.lookup("jms/myQueue");
+			connection = connFactory.createConnection();
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			
+			MessageProducer messageProducer = session.createProducer(queueDestination);
+			messageProducer.send(createJMSMessageForjmsHelloWorldQueue(session, message));
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (session != null) {
+				try {
+					session.close();
+				} catch (JMSException e) {
+					System.err.println("Cannot close session - " + e);
+				}
+			}
+			if (connection != null) {
+				connection.close();
+			}
+		}
+	} 
+    
+    
+    
+    
+    
+    
+    
+    
     
     public void sendMessageByEJB(ActionEvent event){       	
         try {        	
@@ -107,6 +159,8 @@ public class MessageProducerAppMB {
             }
         }
     }
+    
+	  
     
 	public String getMessage() {
 		return message;
