82221450e2b4596449af185f456ed10c5a62c955
fabrizio <fabriziogerina@gmail.com>
2016-04-20 11:38:24 +0200

    rebase

--- AkaJava/src/it/nerd/noob/Reference.java
+++ AkaJava/src/it/nerd/noob/Reference.java
@@ -2,16 +2,15 @@ package it.nerd.noob;
 
 public class Reference {
 	
+		
 	public static void main(String[] args){
 		Bar bar = new Bar();
 		bar.setFooProperty(666);
 		changeBar(bar);
-		System.out.println(bar.getFooProperty());		
-		
+		System.out.println(bar.getFooProperty());	
 	}
 	private static void changeBar(Bar bar){
 		bar.setFooProperty(555);
 	}
-
 }
 
