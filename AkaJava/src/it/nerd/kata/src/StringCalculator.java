11082fe7bdfa72143f6cecf9395e1990a7e6ad71
fabrizio <fabriziogerina@gmail.com>
2016-05-13 16:10:23 +0200

    edited KATA

--- AkaJava/src/it/nerd/kata/src/StringCalculator.java
+++ AkaJava/src/it/nerd/kata/src/StringCalculator.java
@@ -10,76 +10,88 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringCalculator {
-	private static String currNumber;
 
-	/*TEST_GIT*/
+	/* TEST_GIT */
 	/**
-	 * The method can take 0, 1 or 2 numbers, 
-	 * and will return their sum (for an empty string it will return 0) 
-	 * for example “” or “1” or “1,2”
+	 * The method can take 0, 1 or 2 numbers, and will return their sum (for an
+	 * empty string it will return 0) for example “” or “1” or “1,2”
 	 */
-	public static int Add(String text){
+	public int Add(String text) {
 		int numberOne;
 		int numberTwo;
-		if(text.isEmpty()){
+		if (text.isEmpty()) {
 			return 0;
 		}
-		try{
-			String[] numbers = text.split(",");
-			if(numbers.length == 0){
-				return 0;
-			}
-			if(numbers.length == 1){
-				return new Integer(Integer.valueOf(numbers[0]));
-			}
-			if(!numbers[0].isEmpty() && !numbers[1].isEmpty()){
-				 numberOne = new Integer(Integer.valueOf(numbers[0]));
-				 numberTwo = new Integer(Integer.valueOf(numbers[1]));
-				 return numberOne + numberTwo;
-			}else if(!numbers[0].isEmpty()){
-				return  Integer.valueOf(numbers[0]);
-			}else {
-				return  Integer.valueOf(numbers[1]);
-			}			
-		}catch(NumberFormatException ex){
-			return 0;
-		}
-	}	
-	
-	public static int Add2(String text) {
-		String[] numbers = null;
-		List<Long> negativeNnmbers = new ArrayList<Long>();
 		try {
-			if (StringUtils.contains(text, "//") && StringUtils.indexOf(text, "//") == 0) {
-				char delimiter = text.charAt(2);
-				text = StringUtils.replace(text, Character.toString(delimiter), ",");
-			}
-			
-			text = StringUtils.replace(text, "\n", ",");
-			numbers = text.split(",");
-			if (text.isEmpty()) {
+			String[] numbers = text.split(",");
+			if (numbers.length == 0) {
 				return 0;
 			}
-			
 			if (numbers.length == 1) {
 				return new Integer(Integer.valueOf(numbers[0]));
 			}
-			int tot = 0;
-			for (String currNumber : numbers) {
-				if (!NumberUtils.isNumber(currNumber)) {
-					continue;
-				}
-				if(Integer.valueOf(currNumber) < 0){
-					throw new RuntimeException("negatives not allowed: ");
-				}
-				int currParsedNumber = Integer.valueOf(currNumber);
-				tot = tot + currParsedNumber;
+			if (!numbers[0].isEmpty() && !numbers[1].isEmpty()) {
+				numberOne = new Integer(Integer.valueOf(numbers[0]));
+				numberTwo = new Integer(Integer.valueOf(numbers[1]));
+				return numberOne + numberTwo;
+			} else if (!numbers[0].isEmpty()) {
+				return Integer.valueOf(numbers[0]);
+			} else {
+				return Integer.valueOf(numbers[1]);
 			}
-			
-			return tot;
 		} catch (NumberFormatException ex) {
 			return 0;
 		}
+	}
+
+	private String clearString(String text) {
+		String dummyString = text.trim();
+		dummyString = StringUtils.replace(text, "\n", ",");
+		dummyString = dummyString.replaceAll("\\s+","");
+		if (StringUtils.contains(text, "//") && StringUtils.indexOf(text, "//") == 0) {
+			char delimiter = text.charAt(2);
+			dummyString = StringUtils.replace(dummyString, Character.toString(delimiter), ",");
+		}
+		return dummyString;
+	}
+
+	public int Add2(String text) {
+		String[] numbers = null;
+		List<Integer> negativeNnmbers = new ArrayList<Integer>();
+
+		text = clearString(text);
+
+		numbers = text.split(",");
+		if (text.isEmpty()) {
+			return 0;
+		}
+
+		if (numbers.length == 1) {
+			return new Integer(Integer.valueOf(numbers[0]));
+		}
+		int tot = 0;
+		for (String currNumber : numbers) {
+			if (!NumberUtils.isNumber(currNumber)) {
+				continue;
+			}
+			if (Integer.valueOf(currNumber) < 0) {
+				negativeNnmbers.add(Integer.valueOf(currNumber));
+				// throw new IllegalArgumentException("negatives not allowed");
+			} else {
+				int currParsedNumber = Integer.valueOf(currNumber);
+				tot = tot + currParsedNumber;
+			}
+		}
+		if (negativeNnmbers.isEmpty()) {
+			return tot;
+		} else {
+			StringBuffer strBuffErr = new StringBuffer();
+			for (Integer cuurInteger : negativeNnmbers) {
+				strBuffErr.append(cuurInteger);
+			}
+			throw new IllegalArgumentException("negatives not allowed: values are= " + strBuffErr);
+		}
+
 	}
 
 }
--- AkaJava/src/it/nerd/kata/test/StringCalculatorTest.java
+++ AkaJava/src/it/nerd/kata/test/StringCalculatorTest.java
@@ -6,66 +6,55 @@ import java.util.Arrays;
 
 import it.nerd.kata.src.StringCalculator;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class StringCalculatorTest {
-
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
 	@Test
 	public void testAdd() {
-		assertEquals(0, StringCalculator.Add(""));
+		StringCalculator sc = new StringCalculator();
+		assertEquals(0, sc.Add2(""));
 		
-		assertEquals(1, StringCalculator.Add("1"));
-		assertEquals(8, StringCalculator.Add("8"));
-		assertEquals(20, StringCalculator.Add("20"));
+		assertEquals(1, sc.Add2("1"));
+		assertEquals(20, sc.Add2("20"));
+		assertEquals(1, sc.Add2(",1"));
+		assertEquals(20, sc.Add2(",20"));
+		assertEquals(1, sc.Add2("1,"));
+		assertEquals(20, sc.Add2("20,"));
+		assertEquals(20, sc.Add2("20,A"));
+		assertEquals(0, sc.Add2("A,0"));
+		assertEquals(0, sc.Add2("A,A"));
+		assertEquals(2, sc.Add2("1,1"));
+		assertEquals(21, sc.Add2("1,20,"));
 		
-		assertEquals(1, StringCalculator.Add(",1"));
-		assertEquals(8, StringCalculator.Add(",8"));
-		assertEquals(20, StringCalculator.Add(",20"));
-		
-		assertEquals(1, StringCalculator.Add("1,"));
-		assertEquals(8, StringCalculator.Add("8,"));
-		assertEquals(20, StringCalculator.Add("20,"));
-		
-		assertEquals(0, StringCalculator.Add("20,A"));
-		assertEquals(0, StringCalculator.Add("A,0"));
-		assertEquals(0, StringCalculator.Add("A,A"));
-		
-		assertEquals(2, StringCalculator.Add("1,1"));
-		assertEquals(9, StringCalculator.Add("1,8"));
-		assertEquals(21, StringCalculator.Add("1,20"));
-		
-		assertEquals(2, StringCalculator.Add("1,1,9,10"));
-		assertEquals(9, StringCalculator.Add("1,8,3,"));
-		
-		//
-		assertEquals(0, StringCalculator.Add2(""));
-		
-		assertEquals(1, StringCalculator.Add2("1"));
-		assertEquals(8, StringCalculator.Add2("8"));
-		assertEquals(20, StringCalculator.Add2("20"));
-		
-		assertEquals(1, StringCalculator.Add2(",1"));
-		assertEquals(8, StringCalculator.Add2(",8"));
-		assertEquals(20, StringCalculator.Add2(",20"));
-		
-		assertEquals(1, StringCalculator.Add2("1,"));
-		assertEquals(8, StringCalculator.Add2("8,"));
-		assertEquals(20, StringCalculator.Add2("20,"));
-		
-		assertEquals(20, StringCalculator.Add2("20,A"));
-		assertEquals(0, StringCalculator.Add2("A,0"));
-		assertEquals(0, StringCalculator.Add2("A,A"));
-		
-		assertEquals(2, StringCalculator.Add2("1,1"));
-		assertEquals(9, StringCalculator.Add2("1,8"));
-		assertEquals(21, StringCalculator.Add2("1,20,"));
-		
-		assertEquals(21, StringCalculator.Add2("1,1,9,10"));
-		assertEquals(6, StringCalculator.Add2("1\n2,3"));
-		
-		assertEquals(3, StringCalculator.Add2("//;\n1;2"));
-		assertEquals(1, StringCalculator.Add2("1,\n"));
-		
-		assertEquals(3, StringCalculator.Add2("//;\n-1;2"));
+		assertEquals(3, sc.Add2("//;\n1;2"));
+		assertEquals(1, sc.Add2("1,\n"));
+	}
+	
+	@Test
+	public void unknownAmountNumbers(){
+		StringCalculator sc = new StringCalculator();
+		assertEquals(9, sc.Add2("1,1,1,1,5"));
+	}
+	
+	@Test
+	public void newLine(){
+		StringCalculator sc = new StringCalculator();
+		assertEquals(6, sc.Add2("1\n2,3"));
+		assertEquals(5, sc.Add2("\n2,3"));
+		assertEquals(5, sc.Add2("2,3\n"));
+	}
+	
+	@Test
+	public void negativeValue(){
+		thrown.expect(IllegalArgumentException.class);
+		//thrown.expectMessage("negatives not allowed");
+		StringCalculator sc = new StringCalculator();
+		sc.Add2(new String("//;\n-1;2; -3"));
 	}
 }
