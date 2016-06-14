1f701761df00ba8847ef6cc59863e3b45faa5edd
fabrizio <fabriziogerina@gmail.com>
2016-06-14 18:02:54 +0200

    no message

--- AkaJava/src/it/nerd/noob/reflection/Main.java
+++ AkaJava/src/it/nerd/noob/reflection/Main.java
@@ -1,7 +1,10 @@
 package it.nerd.noob.reflection;
 
+import java.net.URL;
+
 public class Main {
 
 	public static void main(String[] args) {
+		URL dummyUrl = Bar.class.getResource("textFile.txt");
 	}
 }
